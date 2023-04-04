0e2fc865912c3a92d0724ad3d59aa4aa0ce994d2
Jingun Hong <jingun.hong@gmail.com>
2023-04-04 17:37:52 +0900

    add nova circle

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
 use nova::NovaPlugin;
 
 mod nova;
@@ -6,11 +7,68 @@ mod nova;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_plugin(WorldInspectorPlugin::new())
         .add_startup_system(setup)
         .add_plugin(NovaPlugin)
         .run();
 }
 
-fn setup(mut commands: Commands) {
-    commands.spawn(Camera2dBundle::default());
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands.spawn(Camera3dBundle {
+        projection: OrthographicProjection {
+            scale: 10.0,
+            scaling_mode: bevy::render::camera::ScalingMode::FixedVertical(2.0),
+            ..default()
+        }
+        .into(),
+        transform: Transform::from_xyz(10.0, 10.0, 10.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
+
+    commands.spawn(PointLightBundle {
+        transform: Transform::from_xyz(0.0, 10.0, 0.0),
+        point_light: PointLight {
+            intensity: 3200.0,
+            color: Color::WHITE,
+            shadows_enabled: true,
+            ..default()
+        },
+        ..default()
+    });
+
+    for x in -10..11 {
+        for z in -10..11 {
+            commands.spawn(PbrBundle {
+                mesh: meshes.add(Mesh::from(shape::Cube { size: 0.99 })),
+                material: materials.add(Color::rgb(1.0, 1.0, 1.0).into()),
+                transform: Transform::from_xyz(x as f32, 0.5, z as f32),
+                ..default()
+            });
+        }
+    }
+
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Cube { size: 0.20 })),
+        material: materials.add(Color::rgb(1.0, 0.05, 0.05).into()),
+        transform: Transform::from_xyz(1.0, 4.0, 1.0),
+        ..default()
+    });
+
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Cube { size: 0.20 })),
+        material: materials.add(Color::rgb(1.0, 0.05, 0.05).into()),
+        transform: Transform::from_xyz(5.0, 4.0, 1.0),
+        ..default()
+    });
+
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Cube { size: 0.20 })),
+        material: materials.add(Color::rgb(1.0, 0.05, 0.05).into()),
+        transform: Transform::from_xyz(1.0, 4.0, -4.0),
+        ..default()
+    });
 }
--- src/nova.rs
+++ src/nova.rs
@@ -3,79 +3,94 @@ use bevy::prelude::*;
 #[derive(Component)]
 pub struct PowerGaugeText;
 
-#[derive(Resource)]
-pub struct Nova {
-    pub power: f32,
-}
-
-impl Default for Nova {
-    fn default() -> Nova {
-        Nova { power: 0.0 }
-    }
-}
+#[derive(Component)]
+pub struct Nova;
 
 pub struct NovaPlugin;
 
 impl Plugin for NovaPlugin {
     fn build(&self, app: &mut App) {
-        app.init_resource::<Nova>()
-            .add_startup_system(init_power_gauge)
-            .add_system(update_power_gauge)
-            .add_system(update_power_value);
+        app.add_startup_system(init_nova)
+            .add_system(move_nova)
+            .add_system(limit_nova_movement)
+            .add_system(update_nova);
     }
 }
 
-fn init_power_gauge(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands
-        .spawn(NodeBundle {
-            style: Style {
-                flex_direction: FlexDirection::Column,
-                justify_content: JustifyContent::Center,
-                align_items: AlignItems::Center,
-                size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
-                margin: UiRect {
-                    top: Val::Percent(10.0),
-                    ..default()
-                },
+fn init_nova(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands.spawn((
+        PbrBundle {
+            mesh: meshes.add(Mesh::from(shape::Cylinder {
+                radius: 0.4,
+                height: 1.0,
+                resolution: 100,
                 ..default()
-            },
+            })),
+            material: materials.add(Color::rgba(0.0, 1.0, 0.0, 0.5).into()),
+            transform: Transform::from_xyz(0.0, 0.5, 0.0),
             ..default()
-        })
-        .with_children(|parent| {
-            parent.spawn((
-                TextBundle::from_section(
-                    "",
-                    TextStyle {
-                        font: asset_server
-                            .load("fonts/Caskaydia Cove Nerd Font Complete Mono Bold.otf"),
-                        font_size: 100.0,
-                        color: Color::WHITE,
-                    },
-                )
-                .with_text_alignment(TextAlignment::Center)
-                .with_style(Style { ..default() }),
-                PowerGaugeText,
-            ));
-        });
+        },
+        Nova,
+    ));
 }
 
-fn update_power_value(keyboard_input: Res<Input<KeyCode>>, mut nova: ResMut<Nova>) {
-    if keyboard_input.pressed(KeyCode::Space) {
-        nova.power += 0.1;
-        if nova.power > 10.0 {
-            nova.power = 10.0;
+fn update_nova(keyboard_input: Res<Input<KeyCode>>, mut query: Query<&mut Transform, With<Nova>>) {
+    if let Ok(mut nova) = query.get_single_mut() {
+        if keyboard_input.pressed(KeyCode::Space) {
+            nova.scale += Vec3::new(0.25, 0.0, 0.25);
+        }
+        if keyboard_input.just_released(KeyCode::Space) {
+            nova.scale = Vec3::new(1.0, 1.0, 1.0);
         }
     }
-    if keyboard_input.just_released(KeyCode::Space) {
-        nova.power = 0.0;
+}
+
+fn move_nova(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut query: Query<&mut Transform, With<Nova>>,
+    time: Res<Time>,
+) {
+    if let Ok(mut nova) = query.get_single_mut() {
+        let mut direction = Vec3::ZERO;
+
+        if keyboard_input.pressed(KeyCode::W) {
+            direction += Vec3::new(-1.0, 0.0, -1.0);
+        }
+        if keyboard_input.pressed(KeyCode::A) {
+            direction += Vec3::new(-1.0, 0.0, 1.0);
+        }
+        if keyboard_input.pressed(KeyCode::S) {
+            direction += Vec3::new(1.0, 0.0, 1.0);
+        }
+        if keyboard_input.pressed(KeyCode::D) {
+            direction += Vec3::new(1.0, 0.0, -1.0);
+        }
+
+        if direction.length() > 0.0 {
+            nova.translation += direction.normalize() * 10.0 * time.delta_seconds();
+        }
     }
 }
 
-fn update_power_gauge(mut query: Query<&mut Text, With<PowerGaugeText>>, nova: Res<Nova>) {
-    if nova.is_changed() {
-        if let Ok(mut text) = query.get_single_mut() {
-            let power = nova.power.round() as usize;
-            text.sections[0].value = "*".repeat(power);
+fn limit_nova_movement(mut query: Query<&mut Transform, With<Nova>>) {
+    if let Ok(mut nova) = query.get_single_mut() {
+        if nova.is_changed() {
+            if nova.translation.x < -10.0 {
+                nova.translation.x = -10.0;
+            }
+            if nova.translation.x > 10.0 {
+                nova.translation.x = 10.0;
+            }
+            if nova.translation.z < -10.0 {
+                nova.translation.z = -10.0;
+            }
+            if nova.translation.z > 10.0 {
+                nova.translation.z = 10.0;
+            }
         }
     }
 }
